91c6c4d07e34a7725d6573b4b061bca4ef6bb9d2
cvargas22 <cesar-ed22@hotmail.com>
2019-10-15 15:30:05 -0300

    modelo pokeDetail y data respectiva

---
+++ lib/src/data/pokemons_detail.dart
@@ -0,0 +1,142 @@
+import 'package:pokedex/src/models/pokedetail.dart';
+
+final PokeDetail bulbasaurDet = PokeDetail(
+  description: "A Bulbasaur es fácil verle echándose una siesta al sol. La semilla que tiene en el lomo va creciendo cada vez más a medida que absorbe los rayos del sol.",
+  height: 0.7,
+  weight: 6.9,
+  percentMale: 87.5,
+  percentFemale: 12.5,
+  eggGroup: "Monstruo, Planta",
+  healthPoints: 45,
+  atack: 49,
+  defense: 49,
+  specialAtack: 65,
+  specialDefense: 65,
+  speed: 45
+);
+final PokeDetail ivysaurDet = PokeDetail(
+  description: "A Bulbasaur es fácil verle echándose una siesta al sol. La semilla que tiene en el lomo va creciendo cada vez más a medida que absorbe los rayos del sol.",
+  height: 0.7,
+  weight: 6.9,
+  percentMale: 87.5,
+  percentFemale: 12.5,
+  eggGroup: "Monstruo, Planta",
+  healthPoints: 45,
+  atack: 49,
+  defense: 49,
+  specialAtack: 65,
+  specialDefense: 65,
+  speed: 45
+);
+final PokeDetail venusaurDet = PokeDetail(
+  description: "A Bulbasaur es fácil verle echándose una siesta al sol. La semilla que tiene en el lomo va creciendo cada vez más a medida que absorbe los rayos del sol.",
+  height: 0.7,
+  weight: 6.9,
+  percentMale: 87.5,
+  percentFemale: 12.5,
+  eggGroup: "Monstruo, Planta",
+  healthPoints: 45,
+  atack: 49,
+  defense: 49,
+  specialAtack: 65,
+  specialDefense: 65,
+  speed: 45
+);
+final PokeDetail charmanderDet = PokeDetail(
+  description: "A Bulbasaur es fácil verle echándose una siesta al sol. La semilla que tiene en el lomo va creciendo cada vez más a medida que absorbe los rayos del sol.",
+  height: 0.7,
+  weight: 6.9,
+  percentMale: 87.5,
+  percentFemale: 12.5,
+  eggGroup: "Monstruo, Planta",
+  healthPoints: 45,
+  atack: 49,
+  defense: 49,
+  specialAtack: 65,
+  specialDefense: 65,
+  speed: 45
+);
+final PokeDetail charmeleonDet = PokeDetail(
+  description: "A Bulbasaur es fácil verle echándose una siesta al sol. La semilla que tiene en el lomo va creciendo cada vez más a medida que absorbe los rayos del sol.",
+  height: 0.7,
+  weight: 6.9,
+  percentMale: 87.5,
+  percentFemale: 12.5,
+  eggGroup: "Monstruo, Planta",
+  healthPoints: 45,
+  atack: 49,
+  defense: 49,
+  specialAtack: 65,
+  specialDefense: 65,
+  speed: 45
+);
+final PokeDetail charizardDet = PokeDetail(
+  description: "A Bulbasaur es fácil verle echándose una siesta al sol. La semilla que tiene en el lomo va creciendo cada vez más a medida que absorbe los rayos del sol.",
+  height: 0.7,
+  weight: 6.9,
+  percentMale: 87.5,
+  percentFemale: 12.5,
+  eggGroup: "Monstruo, Planta",
+  healthPoints: 45,
+  atack: 49,
+  defense: 49,
+  specialAtack: 65,
+  specialDefense: 65,
+  speed: 45
+);
+final PokeDetail squirtleDet = PokeDetail(
+  description: "A Bulbasaur es fácil verle echándose una siesta al sol. La semilla que tiene en el lomo va creciendo cada vez más a medida que absorbe los rayos del sol.",
+  height: 0.7,
+  weight: 6.9,
+  percentMale: 87.5,
+  percentFemale: 12.5,
+  eggGroup: "Monstruo, Planta",
+  healthPoints: 45,
+  atack: 49,
+  defense: 49,
+  specialAtack: 65,
+  specialDefense: 65,
+  speed: 45
+);
+final PokeDetail wartortleDet = PokeDetail(
+  description: "A Bulbasaur es fácil verle echándose una siesta al sol. La semilla que tiene en el lomo va creciendo cada vez más a medida que absorbe los rayos del sol.",
+  height: 0.7,
+  weight: 6.9,
+  percentMale: 87.5,
+  percentFemale: 12.5,
+  eggGroup: "Monstruo, Planta",
+  healthPoints: 45,
+  atack: 49,
+  defense: 49,
+  specialAtack: 65,
+  specialDefense: 65,
+  speed: 45
+);
+final PokeDetail blastoiseDet = PokeDetail(
+  description: "A Bulbasaur es fácil verle echándose una siesta al sol. La semilla que tiene en el lomo va creciendo cada vez más a medida que absorbe los rayos del sol.",
+  height: 0.7,
+  weight: 6.9,
+  percentMale: 87.5,
+  percentFemale: 12.5,
+  eggGroup: "Monstruo, Planta",
+  healthPoints: 45,
+  atack: 49,
+  defense: 49,
+  specialAtack: 65,
+  specialDefense: 65,
+  speed: 45
+);
+final PokeDetail pikachuDet = PokeDetail(
+  description: "A Bulbasaur es fácil verle echándose una siesta al sol. La semilla que tiene en el lomo va creciendo cada vez más a medida que absorbe los rayos del sol.",
+  height: 0.7,
+  weight: 6.9,
+  percentMale: 87.5,
+  percentFemale: 12.5,
+  eggGroup: "Monstruo, Planta",
+  healthPoints: 45,
+  atack: 49,
+  defense: 49,
+  specialAtack: 65,
+  specialDefense: 65,
+  speed: 45
+);
---
+++ lib/src/models/pokedetail.dart
@@ -0,0 +1,32 @@
+
+class PokeDetail{
+  String description;
+  double height;
+  double weight;
+  double percentMale;
+  double percentFemale;
+  String eggGroup;
+  int healthPoints;
+  int atack;
+  int defense;
+  int specialAtack;
+  int specialDefense;
+  int speed;
+
+  PokeDetail({
+    this.description,
+    this.height,
+    this.weight,
+    this.percentMale,
+    this.percentFemale,
+    this.eggGroup,
+    this.healthPoints,
+    this.atack,
+    this.defense,
+    this.specialAtack,
+    this.specialDefense,
+    this.speed
+
+  });
+  
+}
